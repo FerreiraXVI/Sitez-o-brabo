ae631cde6b6a42df14bab6b131d5aa0f19f46cf6
FerreiraXVI <rafaelalmeidaferreira9901@gmail.com>
2025-04-13 22:26:36 -0300

    att

--- js/title.js
+++ js/title.js
@@ -1,5 +1,5 @@
         // Texto que será exibido no <title>
-        const titleText = "Lunox(Luna_detectada)";
+        const titleText = "Lunox(Lana_detectada)";
         let index = 0;
 
         // Função para animar o título
